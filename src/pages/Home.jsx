75877b9655b03104c8d5bf1259b3da8616732389
Elson <elsonooi2005@gmail.com>
2025-12-18 01:30:40 +0800

    Added NeiJuan Ranking

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
-import hanzheng_pic from '../assets/hanzheng.jpg'
 import HeroSection from '../components/HeroSection/HeroSection'
+import NeijuanRanking from '../components/NeijuanRanking/NeijuanRanking';
 import '../css/Home.css'
 
 function Home() {
@@ -9,73 +9,57 @@ function Home() {
             name: "Han Zheng", 
             role: "Founder", 
             desc: "The founder of HanZheng Club", 
-            personality: [
-                "Shuai",
-                "Sao"
-            ]
+            image: "/hanzheng.jpg",
+            personality: ["Shuai", "Sao"]
         },
         {
             name: "Chin Qian", 
             role: "Founder", 
             desc: "The founder of HanZheng Club", 
-            personality: [
-                "Shuai",
-                "Sao"
-            ]
+            image: "/chinqian.jpg",
+            personality: ["Shuai", "Sao"]
         },
         {
             name: "Goo", 
             role: "Founder", 
             desc: "The founder of HanZheng Club", 
-            personality: [
-                "Shuai",
-                "2727"
-            ]
+            image: "/goo.jpg",
+            personality: ["Shuai", "2727"]
         },
         {
             name: "Elson", 
             role: "Founder", 
             desc: "The founder of HanZheng Club", 
-            personality: [
-                "Shuai",
-                "Sao"
-            ]
+            image: "/elson.jpg",
+            personality: ["Shuai", "Sao"]
         },
         {
             name: "Han Lim", 
             role: "Founder", 
             desc: "The founder of HanZheng Club", 
-            personality: [
-                "Shuai",
-                "Sao"
-            ]
+            image: "/hanlim.jpg",
+            personality: ["Shuai", "Sao"]
         },
         {
             name: "Ivan", 
             role: "Founder", 
             desc: "The founder of HanZheng Club", 
-            personality: [
-                "Shuai",
-                "Sao"
-            ]
+            image: "/ivan.jpg",
+            personality: ["Shuai", "Sao"]
         },
         {
             name: "Jun Hao ", 
             role: "Founder", 
             desc: "The founder of HanZheng Club", 
-            personality: [
-                "Sadona",
-                "Sao"
-            ]
+            image: "/junhao.jpg",
+            personality: ["Sadona", "Sao"]
         },
         {
             name: "Kyzer", 
             role: "Founder", 
             desc: "The founder of HanZheng Club", 
-            personality: [
-                "Shuai",
-                "Clash Royale King"
-            ]
+            image: "/kyzer.jpg",
+            personality: ["Shuai", "Clash Royale King"]
         },
     ];
 
@@ -84,14 +68,23 @@ function Home() {
             <HeroSection />
             <section>
                 <h1 className="section-title">HanZheng Club</h1>
+                
                 <div className="member-container">
                     {members.map((item, index) => {
                         return (
-                            <div key={index} className="member-item">
-                                <div className="member-detail">
-                                    <h3>{item.name}</h3>
-                                    <h4>{item.role}</h4>
-                                    <p>{item.desc}</p>
+                            <div key={index} className="member-card">
+                                <img 
+                                    src={item.image} 
+                                    alt={item.name} 
+                                    className="member-picture" 
+                                    onError={(e) => {e.target.src = "/members/default.jpg"}} 
+                                />
+
+                                <h3 className="member-name">{item.name}</h3>
+                                <h4 className="member-role">{item.role}</h4>
+                                <p className="member-desc">{item.desc}</p>
+                                
+                                <div className="personality-container">
                                     {item.personality.map((per, ind) => (
                                         <span key={ind} className="personality-badge">
                                             {per}
@@ -103,7 +96,7 @@ function Home() {
                     })}
                 </div>
             </section>
-        
+            <NeijuanRanking />
         </div>
     )
 }
